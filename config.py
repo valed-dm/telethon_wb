9f868e0bbedf7f6c8bdddb741b9f9e6524716a66
valed-dm <dmvaled@gmail.com>
2024-06-26 16:55:02 +0300

    config.py created

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+# Load environment variables from .env file if present
+load_dotenv()
+
+
+API_ID = int(os.getenv('API_ID'))
+API_HASH = os.getenv('API_HASH')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+BOT_NAME = os.getenv('BOT_NAME')
+SECRET_KEY = os.getenv('SECRET_KEY')
+SESSION_STRING = None
